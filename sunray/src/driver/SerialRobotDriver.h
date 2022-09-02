c5e3ef8149e0bac68852c5317f8db60d24847969
hjw2015 <hj.wolff@gmail.com>
2022-09-02 08:02:42 +0200

    debounce lift sensor

--- sunray/src/driver/SerialRobotDriver.h
+++ sunray/src/driver/SerialRobotDriver.h
@@ -156,6 +156,9 @@ class SerialRainSensorDriver: public RainSensorDriver {
 };
 
 class SerialLiftSensorDriver: public LiftSensorDriver {
+  private:
+    unsigned long nextCheck = 0;
+    int liftCnt = 0;
   public:    
     SerialRobotDriver &serialRobot;
     SerialLiftSensorDriver(SerialRobotDriver &sr);    
